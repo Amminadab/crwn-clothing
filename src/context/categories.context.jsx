5fb6c40a34d9d5cc5ca1a62505bf9bed495d3b23
Amminadab <amminadabeliasfant@gmail.com>
2023-01-28 22:19:02 +0300

    added a new context for categories so that it is easer to fetch products to any page

---
+++ src/context/categories.context.jsx
@@ -0,0 +1,27 @@
+import { createContext, useEffect, useState } from "react";
+
+import { getCategoriesAndDocument } from "../utils/firebase/firebase.utils.js";
+
+export const CategoriesContext = createContext({
+  categoriesMap: {},
+});
+
+export const CategoriesProvider = ({ children }) => {
+  const [categoriesMap, setCategoriesMap] = useState({});
+
+  useEffect(() => {
+    const getCategoryMap = async () => {
+      const categoryMap = await getCategoriesAndDocument();
+      console.log(categoryMap);
+      setCategoriesMap(categoryMap);
+    };
+    getCategoryMap();
+  }, []);
+
+  const value = { categoriesMap };
+  return (
+    <CategoriesContext.Provider value={value}>
+      {children}
+    </CategoriesContext.Provider>
+  );
+};
